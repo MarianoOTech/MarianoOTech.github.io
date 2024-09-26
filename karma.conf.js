0d441b3d2dbf60716416f85ccddba65c7aa6c922
Mariano <159166975+MarianoOTech@users.noreply.github.com>
2024-09-26 09:47:07 -0600

    Update karma.conf.js (#34)

--- karma.conf.js
+++ karma.conf.js
@@ -38,6 +38,7 @@ module.exports = function (config) {
           ChromeHeadlessCI: {
            base: 'ChromeHeadless',
           flags: ['--no-sandbox']
+        }
       },
     singleRun: false,
     restartOnFileChange: true
